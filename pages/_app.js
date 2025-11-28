aa8c13812ba04b743b1f6207228f9de41db9335a
Denis mec <denisfilho2@gmail.com>
2025-11-28 11:14:46 -0300

    icone barra navegação!

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,9 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Ação Saúde - Eu Faço o Bem</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/imagens/coracao-laranja.png" type="image/png" />
+        <link rel="icon" href="/coracao-laranja-32.png" type="image/png" />
+
+
 
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
